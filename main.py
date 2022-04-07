73675ef776fc06843eae3e990e15eefad0a1d9a9
Štěpán Kakeš <91247524+StepanKakes@users.noreply.github.com>
2022-04-07 20:58:41 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,30 @@
+stred=[2,2]
+A=[2,1]
+B=[0,2]
+C=[3,2]
+def on_forever():
+    led.plot_brightness(stred[0], stred[1], 100)
+    led.plot(A[0],A[1])
+    led.plot(B[0],B[1])
+    led.plot(C[0],C[1])
+    
+basic.forever(on_forever)
+def on_button_pressed_a():
+    otaceni(0)
+def on_button_pressed_b():
+    otaceni(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+def otaceni(y):
+    global A, B, C
+    basic.clear_screen()
+    A.reverse()
+    A[y]= abs(A[y]-4)
+    B.reverse()
+    B[y]= abs(B[y]-4)
+    C.reverse()
+    C[y]= abs(C[y]-4)
+    
+    
+    #B.reverse()
+    #C.reverse()
+input.on_button_pressed(Button.A, on_button_pressed_a)
---
+++ main.ts
@@ -0,0 +1,29 @@
+let stred = [2, 2]
+let A = [2, 1]
+let B = [0, 2]
+let C = [3, 2]
+basic.forever(function on_forever() {
+    led.plotBrightness(stred[0], stred[1], 100)
+    led.plot(A[0], A[1])
+    led.plot(B[0], B[1])
+    led.plot(C[0], C[1])
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    otaceni(1)
+})
+function otaceni(y: number) {
+    
+    basic.clearScreen()
+    A.reverse()
+    A[y] = Math.abs(A[y] - 4)
+    B.reverse()
+    B[y] = Math.abs(B[y] - 4)
+    C.reverse()
+    C[y] = Math.abs(C[y] - 4)
+}
+
+// B.reverse()
+// C.reverse()
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    otaceni(0)
+})

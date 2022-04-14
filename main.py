10e2e100dac3901676c6ff8f5fa44857b1997396
Štěpán Kakeš <91247524+StepanKakes@users.noreply.github.com>
2022-04-14 17:18:09 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ A=[2,1]
 B=[0,2]
 C=[3,2]
 def on_forever():
+    display()
+    
+basic.forever(on_forever)
+def display():
+    global stred,A,B,C
+    basic.clear_screen()
     led.plot_brightness(stred[0], stred[1], 100)
     led.plot(A[0],A[1])
     led.plot(B[0],B[1])
     led.plot(C[0],C[1])
-    
-basic.forever(on_forever)
 def on_button_pressed_a():
     otaceni(0)
 def on_button_pressed_b():
@@ -16,7 +20,6 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 def otaceni(y):
     global A, B, C
-    basic.clear_screen()
     A.reverse()
     A[y]= abs(A[y]-4)
     B.reverse()
--- main.ts
+++ main.ts
@@ -3,17 +3,22 @@ let A = [2, 1]
 let B = [0, 2]
 let C = [3, 2]
 basic.forever(function on_forever() {
+    display()
+})
+function display() {
+    
+    basic.clearScreen()
     led.plotBrightness(stred[0], stred[1], 100)
     led.plot(A[0], A[1])
     led.plot(B[0], B[1])
     led.plot(C[0], C[1])
-})
+}
+
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     otaceni(1)
 })
 function otaceni(y: number) {
     
-    basic.clearScreen()
     A.reverse()
     A[y] = Math.abs(A[y] - 4)
     B.reverse()
